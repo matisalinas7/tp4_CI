fd827a165ce51e9feacb42cb099df30f14085200
Matias Salinas <matisalinas.2000@gmail.com>
2025-06-23 08:40:03 -0300

    Estructura inicial tp4_CI

---
+++ src/main/java/com/ejemplo/StringUtils.java
@@ -0,0 +1,18 @@
+package com.ejemplo;
+
+public class StringUtils {
+    public static int contarVocales(String texto) {
+        // TODO: Implementar
+        return 0;
+    }
+
+    public static String invertir(String texto) {
+        // TODO: Implementar
+        return null;
+    }
+
+    public static boolean esPalindromo(String texto) {
+        // TODO: Implementar
+        return false;
+    }
+} 
---
+++ src/test/java/com/ejemplo/StringUtilsTest.java
@@ -0,0 +1,21 @@
+package com.ejemplo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StringUtilsTest {
+    @Test
+    public void testContarVocales() {
+        // TODO: Implementar prueba
+    }
+
+    @Test
+    public void testInvertir() {
+        // TODO: Implementar prueba
+    }
+
+    @Test
+    public void testEsPalindromo() {
+        // TODO: Implementar prueba
+    }
+} 
