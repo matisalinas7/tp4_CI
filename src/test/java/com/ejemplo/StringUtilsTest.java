d544c783dcf0d678c9c82b8ec8a1c36b34072abe
Matias Salinas <matisalinas.2000@gmail.com>
2025-06-26 12:43:21 -0300

    pruebas fail para video

--- src/test/java/com/ejemplo/StringUtilsTest.java
+++ src/test/java/com/ejemplo/StringUtilsTest.java
@@ -22,15 +22,15 @@ public class StringUtilsTest {
     }
 
     // ContarVocales incorrectos
-    // @Test
-    // public void testContarVocales_incorrecto_esperaMas() {
-    //     assertEquals(10, StringUtils.contarVocales("Hola mundo"));
-    // }
+    @Test
+    public void testContarVocales_incorrecto_esperaMas() {
+        assertEquals(3, StringUtils.contarVocales("Hola mundo"));
+    }
 
-    // @Test
-    // public void testContarVocales_incorrecto_esperaMenos() {
-    //     assertEquals(0, StringUtils.contarVocales("aeiou"));
-    // }
+    @Test
+    public void testContarVocales_incorrecto_esperaMenos() {
+        assertEquals(2, StringUtils.contarVocales("aeiou"));
+    }
 
     // @Test
     // public void testContarVocales_incorrecto_vacioEsUno() {
